933829e29acd0ebece8704bb804d635bb8bc9b8a
binrambler <104247888+binrambler@users.noreply.github.com>
2022-05-27 22:47:20 +0400

    Add files via upload

--- duhi.py
+++ duhi.py
@@ -4,69 +4,78 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-import duhi_query
 
-server = '192.168.20.5'
-database = 'IZH_SQL_2018'
-username = 'sa'
-password = ''
-with pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-    s_sql = duhi_query.query_sale("'20180101'", "'20220430'", "'  2KL0   '")
-    df = pd.read_sql(s_sql, conn)
-    # print(df)
-# exit()
+WORK_DIR = pathlib.Path('d:/py/ml')
+CURR_DIR = pathlib.Path.cwd()
+FILE_XLS = pathlib.Path(WORK_DIR, 'ml_духи.xls')
+FILE_GPH = pathlib.Path(WORK_DIR, 'ml_духи.png')
+FILE_QRY = pathlib.Path(CURR_DIR, 'query.txt')
 
-path_dir = pathlib.Path('d:/py/ml')
-path_xls = pathlib.Path(path_dir, 'ml_духи.xls')
-path_plt = pathlib.Path(path_dir, 'ml_духи.png')
+SERVER = '192.168.20.5'
+DATABASE = 'IZH_SQL_2018'
+USERNAME = 'sa'
+PASSWORD = ''
 
-# читаем файл, подготавливаем данные
-# df = pd.read_excel(io=path_xls, engine='xlrd')
-df['Сумма'] = round(df['Сумма'] / 1000, 0)
+dB = "'20180101'"
+dE = "'20220430'"
+gruppa_id = "'  2KL0   '"
+
+def read_query():
+    with open(FILE_QRY, 'r', encoding='utf-8-sig') as f:
+        str_qry = ''.join([s for s in f])
+    str_qry = str_qry.replace('<dB>', dB)
+    str_qry = str_qry.replace('<dE>', dE)
+    str_qry = str_qry.replace('<gruppa_id>', gruppa_id)
+    return str_qry
+
+def exec_query(str_qry):
+    with pyodbc.connect('DRIVER={SQL Server};SERVER=' + SERVER +
+                        ';DATABASE=' + DATABASE +
+                        ';UID=' + USERNAME +
+                        ';PWD=' + PASSWORD) as conn:
+        return pd.read_sql(str_qry, conn)
 
 def graph(arr):
     # строим график продаж по месяцам
     fig, ax = plt.subplots()
     fig.set_size_inches(8, 6)
     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-    sns.lineplot(data=arr, x='Месяц', y='Сумма', hue='Год', palette='plasma')
+    sns.lineplot(data=arr, x='MONTH', y='SUMMA', hue='YEAR', palette='plasma')
     plt.grid(True)
-    plt.title('Продажи по месяцам')
+    plt.title('Продажи')
+    plt.xlabel('Месяц')
     plt.ylabel('Сумма, тыс. руб.')
-    plt.savefig(path_plt, dpi = 150)
+    plt.savefig(FILE_GPH, dpi = 150)
 
 def predict(arr, year, month):
-    X = arr.query('Месяц == @month')
-    y = X.loc[:, 'Сумма'].values
-    X = X.loc[:, 'Год'].values.reshape(-1, 1)
-    # model = LinearRegression()
+    X = arr.query('MONTH == @month')
+    y = X.loc[:, 'SUMMA'].values
+    X = X.loc[:, 'YEAR'].values.reshape(-1, 1)
     model = RandomForestRegressor()
     model.fit(X, y)
     X_new = [[year]]
     return round(model.predict(X_new)[0], 0)
 
-def fill(arr):
+def fill_array(arr):
     while 1:
-        yr = max(arr['Год'])
-        mn = max(arr.query('Год == @yr')['Месяц']) + 1
+        yr = max(arr['YEAR'])
+        mn = max(arr.query('YEAR == @yr')['MONTH']) + 1
         if mn > 12:
             break
         pred = predict(arr, yr, mn)
-        data = pd.DataFrame({'Год': [yr],
-                            'Месяц': [mn],
-                            'Сумма': [pred]})
+        data = pd.DataFrame({'YEAR': [yr],
+                            'MONTH': [mn],
+                            'SUMMA': [pred]})
         arr = pd.concat([arr, data], ignore_index=True)
     return arr
 
+str_qry = read_query()
+arr = exec_query(str_qry)
+# читаем файл, подготавливаем данные
+# df = pd.read_excel(io=FILE_XLS, engine='xlrd')
+arr['SUMMA'] = round(arr['SUMMA'] / 1000, 0)
 # graph(df)
-arr = fill(df)
+arr = fill_array(arr)
 print(arr)
 graph(arr)
-
-
-
-
-
-
